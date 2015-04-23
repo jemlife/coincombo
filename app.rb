10cb49337fb1cfd51651636ab9c26904c9a61361
Jeremy Greenwood & Brent Bailey <student@epicodus.com>
2015-04-23 10:47:58 -0700

    Add erb files to views folder, fill out

---
+++ app.rb
@@ -0,0 +1,13 @@
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/coin_combo')
+also_reload('lib/**/*.rb')
+
+get('/') do
+erb(:index)
+end
+
+get ('/result') do
+  @result = params.fetch('input').coin_combo()
+  erb(:result)
+end
