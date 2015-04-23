b961ae0443fa12ae4663f48c172482bbb2e9fa32
Jeremy Greenwood & Brent Bailey <student@epicodus.com>
2015-04-23 10:05:41 -0700

    Add and passs specs for quarter, dimes and nickels.

---
+++ lib/coin_combo.rb
@@ -0,0 +1,28 @@
+class Fixnum
+  define_method(:coin_combo) do
+    quarter_amount = 0
+    dime_amount = 0
+    nickel_amount = 0
+    counter = self
+    until counter == 0
+      if counter >= 25
+        while counter >= 25 do
+          counter = counter - 25
+          quarter_amount = quarter_amount + 1
+        end
+    elsif counter >= 10
+      while counter >= 10 do
+        counter = counter - 10
+        dime_amount = dime_amount + 1
+      end
+    elsif counter >= 5
+      while counter >= 5 do
+        counter = counter - 5
+       nickel_amount = nickel_amount + 1
+    else
+
+    end
+  end
+    "Your change is #{quarter_amount} quarters and #{dime_amount} dimes."
+  end
+end
---
+++ spec/coin_combo_spec.rb
@@ -0,0 +1,16 @@
+require('rspec')
+require('coin_combo')
+
+describe('Fixnum#coin_combo') do
+it('takes an amount of cents and shows the smallest number of quarters needed to make change') do
+  expect(125.coin_combo()).to(include('5 quarters'))
+end
+
+it('takes an amount of cents and shows the smallest number of dimes needed to make change') do
+  expect(20.coin_combo()).to(include('2 dimes'))
+end
+
+it('taskes an amount of cents and show the smallest numbe rof nickels needed to make change') do
+  expect(5.coin_combo()).to(include('1 nickel'))
+end
+end
